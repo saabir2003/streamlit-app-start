b5147c9b55f3f9ea2876a46217090d7beca2f499
saabir <122335172+saabir2003@users.noreply.github.com>
2024-09-22 10:46:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title('🤖 machine learning app')
 
 st.write('facial emotion classification using tensorflow and transfer learning')
-st.image('angry.jpeg')
+st.image('angry.jpeg', caption="an angry person")
