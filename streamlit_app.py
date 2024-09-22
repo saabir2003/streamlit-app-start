8dc47e7d162ab8c42c02ea33624a7db33013875f
saabir <122335172+saabir2003@users.noreply.github.com>
2024-09-22 10:50:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
+from PIL import Image
+
 
 st.title('🤖 machine learning app')
 
 st.write('facial emotion classification using tensorflow and transfer learning')
 st.image('angry.jpeg', caption="an angry person")
+
+
+# Title of the app
+st.title("Image Upload and Display")
+
+# File uploader for images
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    # Open the image using PIL
+    image = Image.open(uploaded_file)
+    
+    # Display the image
+    st.image(image, caption='Uploaded Image', use_column_width=True)
