875a5a30b6dadc721cd55b2b08c9a37807ed2fb6
saabir <122335172+saabir2003@users.noreply.github.com>
2024-09-23 18:50:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from PIL import Image
-!git clone https://github.com/parth1620/Facial-Expression-Dataset.git
-!pip install -U git+https://github.com/albumentations-team/albumentations
-!pip install timm
-!pip install --upgrade opencv-contrib-python
+git clone https://github.com/parth1620/Facial-Expression-Dataset.git
+pip install -U git+https://github.com/albumentations-team/albumentations
+pip install timm
+pip install --upgrade opencv-contrib-python
 
 st.title('🤖 machine learning app')
 
